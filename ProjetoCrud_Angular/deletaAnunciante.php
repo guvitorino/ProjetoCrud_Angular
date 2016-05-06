ba6659d9615881052f444db27ff6582417404c4b
Gustavo Vitorino <gu_vitorino@hotmail.com>
2016-05-06 16:42:46 -0300

    adicionado deletar no banco

--- ProjetoCrud_Angular/deletaAnunciante.php
+++ ProjetoCrud_Angular/deletaAnunciante.php
@@ -2,15 +2,11 @@
 	include "conectar.php";
 ?>
 <?php
-	$sql = 'DELETE from "Anunciante" where idanunciante='.$_POST[id];  
+ 	$dados_table = file_get_contents("php://input");
+    $obj = json_decode($dados_table);
+
+	$id = $obj->id;
+	$sql = 'DELETE from "Anunciante" where idanunciante='.$id;  
 	$sql_query = pg_query($conect,$sql) or die(pg_last_error($conect));
 	pg_close($conect); 
-	setcookie("nome", "", time()-3600);
-	setcookie("logado", "", time()-3600);
-	setcookie("id", "", time()-3600);
-	unset($_COOKIE['nome']);
-	unset($_COOKIE['logado']);
-	unset($_COOKIE['id']);
 ?>
-<script type="text/javascript"> alert("Seus dados foram deletados!");
-window.location.replace('home.html'); </script>
